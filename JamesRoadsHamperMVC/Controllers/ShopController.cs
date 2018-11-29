e2da6b3a26432639cc3161045bffaad23fa16476
james roads <james.roads51@gmail.com>
2018-11-29 13:30:40 +1100

    Add controller and vm

---
+++ JamesRoadsHamperMVC/Controllers/ShopController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//...
+using Microsoft.AspNetCore.Identity;
+using JamesRoadsHamperMVC.Models;
+using JamesRoadsHamperMVC.ViewModels;
+using JamesRoadsHamperMVC.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JamesRoadsHamperMVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        private IDataService<Hamper> _hamperService;
+        public ShopController(IDataService<Hamper> hamperService)
+        {
+            _hamperService = hamperService;
+        }
+        [HttpGet]
+        public IActionResult Shop()
+        {
+            //call service
+            IEnumerable<Hamper> hamperList = _hamperService.GetAll();
+
+            //create VM
+            ShopShopViewModel vm = new ShopShopViewModel
+            {
+                Hampers = hamperList,
+
+            };
+
+            return View();
+        }
+        
+    }
+}
---
+++ JamesRoadsHamperMVC/ViewModels/ShopShopViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JamesRoadsHamperMVC.Models;
+
+namespace JamesRoadsHamperMVC.ViewModels
+{
+    public class ShopShopViewModel
+    {
+        public int Total { get; set; }
+        public IEnumerable<Hamper> Hampers { get; set; }
+    }
+}
